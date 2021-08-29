d6e60489ff48e0eb8b8bdd2650e5daa90717a6f6
syedmuhammadzulqarnain2@gmail.com <syedmuhammzulqarnain2@gmail.com>
2021-08-29 12:09:04 +0200

    app semi completed, with core functionality operating.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import data from "./data";
 import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
     <main className="">
       <section className="container">
-        <h3>0 birthdays today</h3>
-        <List />
-        <button onClick={() => console.log("you clicked me")}>clear all</button>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
   );
